3cbd04873b12863d3ed31738f42fe80c0c39b04d
klangner <klangner@gmail.com>
2017-04-16 08:58:01 +0200

    Update to stats 0.1.0

--- src/Learn/Unsupervised/OutlierClassifier.purs
+++ src/Learn/Unsupervised/OutlierClassifier.purs
@@ -8,9 +8,10 @@ module Learn.Unsupervised.OutlierClassifier
   ) where
 
 import Prelude
+import Data.Maybe (fromMaybe)
 import LinearAlgebra.Matrix (Matrix, columns, rows)
 import LinearAlgebra.Vector (Vector)
-import Statistics.Distribution.Normal (NormalDistribution, fromSample)
+import Statistics.Distribution.Normal (NormalDistribution, fromSample, standard)
 
 
 -- | Model based on normal distribution for each feature 
@@ -19,7 +20,8 @@ data Model = Model (Array NormalDistribution)
 
 -- Convert timestamp in seconds into the DateTime
 learn :: Matrix Number -> Model
-learn xs = Model $ fromSample <$> columns xs
+learn xs = Model $ mkDistr <$> columns xs
+  where mkDistr = (fromMaybe standard <<< fromSample)
 
 
 -- | Predict values based on learned model.
