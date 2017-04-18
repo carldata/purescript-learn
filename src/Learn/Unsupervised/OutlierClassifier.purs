9a21d29a7c117bf07b968456e7f7ee5aff9d7f0b
Krzysztof Langner <klangner@gmail.com>
2017-04-18 10:24:27 +0200

    Implemented first version of outlier detection

--- src/Learn/Unsupervised/OutlierClassifier.purs
+++ src/Learn/Unsupervised/OutlierClassifier.purs
@@ -5,17 +5,23 @@ module Learn.Unsupervised.OutlierClassifier
   ( Model
   , learn
   , predict
+  , predict1
   ) where
 
 import Prelude
+import Data.Array as A
 import Data.Maybe (fromMaybe)
 import LinearAlgebra.Matrix (Matrix, columns, rows)
 import LinearAlgebra.Vector (Vector)
+import Statistics.Distribution (density)
 import Statistics.Distribution.Normal (NormalDistribution, fromSample, standard)
 
 
 -- | Model based on normal distribution for each feature 
 data Model = Model (Array NormalDistribution)
+
+instance showModel :: Show Model where
+  show (Model ds)= "Model: " <> show ds
 
 
 -- Convert timestamp in seconds into the DateTime
@@ -31,6 +37,6 @@ predict m xs = predict1 m <$> rows xs
 
 -- | Predict value for a single sample
 predict1 :: Model -> Vector Number -> Number
-predict1 m v = 0.0
+predict1 (Model ds) v = A.foldl (*) 1.0 $ A.zipWith density ds v
   
     
--- test/Test/Learn/Unsupervised/OutlierClassifier.purs
+++ test/Test/Learn/Unsupervised/OutlierClassifier.purs
@@ -17,7 +17,8 @@ testOutlierClassifier = do
 
     log "build model"
     let test1 = unsafePartial $ fromJust $ M.fromArray 5 1 [1.0, 1.1, 0.9, 1.2, 1.0]
-    let validate1 = unsafePartial $ fromJust $ M.fromArray 4 1 [1.0, 1.1, 90.0, 1.2]
+    let validate1 = unsafePartial $ fromJust $ M.fromArray 4 1 [1.0, 0.8, 9.0, 1.2]
     let model = learn test1
     let y1 = predict model validate1
+    log $ show y1
     assert $ A.length y1 == 4
